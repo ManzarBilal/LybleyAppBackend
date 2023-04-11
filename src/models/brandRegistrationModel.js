2bf683f2a9aa9e7c86860d3254d40bbe2069b476
ManzarBilal <manzarbilal@gmail.com>
2023-04-11 11:42:15 +0530

    second commit

--- src/models/brandRegistrationModel.js
+++ src/models/brandRegistrationModel.js
@@ -5,10 +5,10 @@ const brandSchema= new mongoose.Schema({
     email:{type:String,required:true},
     contact:{type:Number,required:true},
     password:{type:String,required:true},
-   // gstNo:{type:String,required:true},
+    gstNo:{type:String,required:true},
     gstDocument:{type:String,required:true},
-    // brandLogo:{type:String},
-    // brandBanner:{type:String},
+    brandLogo:{type:String},
+    brandBanner:{type:String},
      otp:{type:String},
     status:{type:String,default:"INACTIVE"},
     role:{type:String,default:"BRAND"}
