1db05feddfae780a090e064ffbe5631189165f62
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2023-04-28 17:36:11 +0530

    ;kdhgc

--- src/models/videoModel.js
+++ src/models/videoModel.js
@@ -1,6 +1,7 @@
 const mongoose=require("mongoose");
 
 const videoUpload=new mongoose.Schema({
+    brandId:{type:String,required:true},
     video:{type:String,required:true},
     productModel:{type:String,required:true},
 },{timestamps:true})
