fcbc8878cc35267eb28f248b489f422bac538f4a
ManzarBilal <manzarbilal@gmail.com>
2023-05-09 10:35:38 +0530

    first

--- src/models/order.js
+++ src/models/order.js
@@ -2,6 +2,7 @@ const mongoose=require("mongoose");
 
 const orderSchema=new mongoose.Schema({
      name:{type:String,required:true},
+     customerId:{type:String,required:true},
      contact:{type:String,required:true},
      email:{type:String},
      address:{type:String,required:true},
