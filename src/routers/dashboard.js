ba777413a125df51dc9b3b566821dd8bc4ce6fc1
rajeshtiwari2503 <rajeshtiwari2503@gmail.com>
2023-05-10 17:42:48 +0530

    bdgfg

--- src/routers/dashboard.js
+++ src/routers/dashboard.js
@@ -3,16 +3,21 @@ const router=new express.Router();
 const Customers=require("../models/userRegistrationModel");
 const Orders=require("../models/order");
 const SpareParts=require("../models/sparePartsModel");
+const Brand =require("../models/brandRegistrationModel")
 
 router.get("/dashboardDetails",async(req,res)=>{
      try{
           let customer=await Customers.find({});
           let order=await Orders.find({});
           let sparePart=await SpareParts.find({});
+          let brand=await Brand.find({})
           res.json({
             totalCustomers:customer.length,
             totalOrders:order.length,
-            sparePart:sparePart.length
+            sparePart:sparePart.length,
+            totalBrands:brand.length-1,
+            orders:order,
+            sparParts:sparePart,
           });
      }catch(err){
           res.status(400).send(err);
