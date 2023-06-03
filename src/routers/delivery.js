9684f5c94fbfec883e85c78631c3be92f2a8e118
ManzarBilal <manzarbilal@gmail.com>
2023-06-03 17:16:28 +0530

    dfgh

--- src/routers/delivery.js
+++ src/routers/delivery.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const router=new express.Router();
 const axios=require("axios");
+const Order=require("../models/order");
 
 const token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjM1OTEyMTcsImlzcyI6Imh0dHBzOi8vYXBpdjIuc2hpcHJvY2tldC5pbi92MS9leHRlcm5hbC9hdXRoL2xvZ2luIiwiaWF0IjoxNjg0OTk2NjMyLCJleHAiOjE2ODU4NjA2MzIsIm5iZiI6MTY4NDk5NjYzMiwianRpIjoiWnZsS0dtYkZhNXNHMWJ5MSJ9.1AHfH1T3xFFR-e1hz20WVgk2iQq1_taNYeMwRr54cPE";
 
@@ -16,6 +17,7 @@ router.post("/createDeliveryOrder",async(req,res)=>{
           let body=req.body;
           let response=await axios.post("https://apiv2.shiprocket.in/v1/external/orders/create/adhoc",body,{headers:{'Authorization':`Bearer ${token}`}});
           let {data}=response;
+          await Order.updateOne({_id:body.order_id},{shipOrderId:data?.order_id,shipmentId:data?.shipment_id});
           res.send(data);
        }catch(err){
           res.status(400).send(err.response.data);
